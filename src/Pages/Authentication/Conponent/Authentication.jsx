c2e441e95ab5f7f9d43e4e2e7ec4c724aa3d385b
Debbie <119403063+debby71711@users.noreply.github.com>
2023-07-25 13:50:16 +0100

    initial commit:

---
+++ src/Pages/Authentication/Conponent/Authentication.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../Authentication/Style/Authentication.css"
+export const Authentication=()=>{
+    return(
+        <>
+        <div className="AuthenticationMainContainer">
+            <div className="AuthenticationSemiMain">
+                Enter Otp
+            <input
+            type="digit"
+            id="emailInput"
+            className="AuthenticationEmail"
+            minLength={11}
+            name="emailAddress"
+          />
+          <div className="AuthenticationEmail"> Login</div>
+            </div>
+            </div>
+        </>
+    )
+}
---
+++ src/Pages/LandingPage/Component/LandingPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../LandingPage/Style/LandingPage.css";
+export const LandingPage=()=>{
+    return(
+        <>
+        <div className="LandingPage">
+            <div className="LandingPageTopNav">
+            <div className="LandingPageTopNavText">
+                <li>SignUp</li>
+                <li>LogIn</li>
+                <li>Contact</li>
+                <li>AboutUs</li>
+                </div>
+            </div>
+        <div className="LandingPageMainContainer">
+                <div className="BlueBox">
+                    <h1 className="LandingPageBigText">Imagine</h1>
+                    <h2 className="LandingPageBigText2">a caregiver who doesn't just "watch" <p>over your child but truly becomes a</p> part of their world </h2>
+                    <div className="LandingPageBlackButton">
+                    Find a CareTaker
+                    </div>
+                </div>
+                <div className="ImageBox"> </div>
+        </div>
+        </div>
+        </>
+    )
+}
